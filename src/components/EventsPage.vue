1e0c3337070a2419e593790d5502d78901ddac7c
Hasan Sezai Turkyilmaz <hsturkyilmaz@gmail.com>
2022-06-19 00:33:52 +0300

    Develop search handling

--- src/components/EventsPage.vue
+++ src/components/EventsPage.vue
@@ -1,9 +1,46 @@
 <template>
   <div>EVENTS PAGE</div>
+  <Search @SearchRequested="handleSearch"></Search>
+  Loading: {{ isLoading }} <br />
+  Events: {{ events }}
 </template>
 <script>
+import Search from "./Search.vue";
+import axios from "axios";
+
 export default {
   name: "EventsPage",
+  components: {
+    Search,
+  },
+  data() {
+    return {
+      isLoading: true,
+      events: [],
+    };
+  },
+  methods: {
+    async handleSearch(query) {
+      this.events = [];
+      this.isLoading = true;
+      try {
+        const response = await axios.get(
+          `https://app.ticketmaster.com/discovery/v2/events.json`,
+          {
+            params: {
+              keyword: query,
+              apikey: "3tzBg3PBl12PnGMbbNAnRMKOdQDlAAJi",
+            },
+          }
+        );
+        console.log(response.data);
+        this.events = response.data._embedded.events[0].name;
+        this.isLoading = false;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
 };
 </script>
 <style scoped>
---
+++ src/components/Search.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="search">
+    <input type="text" v-model="query" @keypress.enter="handleSearch" />
+    <button @click="handleSearch">Search</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {
+      query: "",
+    };
+  },
+  methods: {
+    handleSearch() {
+      this.$emit("SearchRequested", this.query);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.search input {
+  padding: 10px 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  width: 400px;
+}
+
+.search button {
+  padding: 5px 10px;
+  border: 1px solid #ccc;
+  background-color: #ccc;
+  color: #fff;
+  border-radius: 5px;
+}
+</style>  
